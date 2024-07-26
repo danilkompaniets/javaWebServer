12aa94327373f47f9ef80ae2ba45cb8f5512ca20
danilkompaniets <kompaniets1592925@gmail.com>
2024-07-26 13:23:43 +0300

    multithreading added

--- src/main/java/com/danilkompaniets/server/core/ServerListenerThread.java
+++ src/main/java/com/danilkompaniets/server/core/ServerListenerThread.java
@@ -1,8 +1,45 @@
 package com.danilkompaniets.server.core;
 
+import com.danilkompaniets.server.HttpServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Logger;
+
+
 public class ServerListenerThread extends Thread {
+    private final static Logger LOGGER = Logger.getLogger(HttpServer.class.getName());
+
+    private int port ;
+    private String webroot;
+    private ServerSocket serverSocket;
+
+    public ServerListenerThread(int port, String webroot) throws IOException {
+        this.port = port;
+        this.webroot = webroot;
+        this.serverSocket = new ServerSocket(this.port);
+    }
+
     @Override
     public void run() {
-        super.run();
+        while (serverSocket.isBound() && !serverSocket.isClosed()) {
+            try {
+                Socket socket = serverSocket.accept();
+                LOGGER.info("* Connected -> " + socket.getInetAddress());
+                HttpConnectionWorkerThread workerThread = new HttpConnectionWorkerThread(socket);
+                workerThread.start();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (serverSocket != null && !serverSocket.isClosed()) {
+                    try {
+                        serverSocket.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
     }
 }
